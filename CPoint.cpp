ec9c64a2cb40439f09244c3febfc225c275c4f9a
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-09-08 10:41:48 +0300

    The CPoint class was created.

---
+++ CPoint.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CPoint.h"
+
+CPoint::CPoint(int _i,double _width,double _height)
+{
+	i = _i;
+
+	width = _width;
+	height = _height;
+
+	frequency = 1;
+
+	amplitude = height / 2;
+	centerY = height / 2;
+
+}
+
+double CPoint::X()
+{
+	return (2 * pi * i) / (points_count - 1);
+}
+double CPoint::Y()
+{
+	return centerY + amplitude * sin(frequency * X());
+}
---
+++ CPoint.h
@@ -0,0 +1,30 @@
+#pragma once
+#include"SinusoidDoc.h"
+
+#define Point CPoint
+#define Document CSinusoidDoc
+#define Rect CRect
+class CPoint
+{
+private:
+	
+
+	double width, height;
+	double centerY;
+
+	double amplitude, frequency;
+
+	int i;
+
+public :
+	const int points_count = 40;
+	const double pi = 3.14159265358979323846;
+
+	double X();
+	double Y();
+
+	CPoint(int _i,double _width,double height);
+
+
+	
+};
