d83a4918318eab5dd561ecf304946b8f1873fd77
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-09-14 08:13:14 +0300

    The CControlTreeView class was created.

---
+++ CControlTreeView.cpp
@@ -0,0 +1,43 @@
+﻿// CControlTreeView.cpp: файл реализации
+//
+
+#include "pch.h"
+#include "Sinusoid.h"
+#include "CControlTreeView.h"
+
+
+// CControlTreeView
+
+IMPLEMENT_DYNCREATE(CControlTreeView, CTreeView)
+
+CControlTreeView::CControlTreeView()
+{
+
+}
+
+CControlTreeView::~CControlTreeView()
+{
+}
+
+BEGIN_MESSAGE_MAP(CControlTreeView, CTreeView)
+END_MESSAGE_MAP()
+
+
+// Диагностика CControlTreeView
+
+#ifdef _DEBUG
+void CControlTreeView::AssertValid() const
+{
+	CTreeView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CControlTreeView::Dump(CDumpContext& dc) const
+{
+	CTreeView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// Обработчики сообщений CControlTreeView
---
+++ CControlTreeView.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include"afxcview.h"
+
+// Просмотр CControlTreeView
+
+class CControlTreeView : public CTreeView
+{
+	DECLARE_DYNCREATE(CControlTreeView)
+
+protected:
+	CControlTreeView();           // защищенный конструктор, используемый при динамическом создании
+	virtual ~CControlTreeView();
+
+public:
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
