fb10fa630c287aa9090e5f5c46922f5b31fa8495
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-10-07 17:07:51 +0300

    The dialog window was added to the project.

--- Resource.h
+++ Resource.h
@@ -6,6 +6,7 @@
 #define IDP_OLE_INIT_FAILED             100
 #define IDR_MAINFRAME                   128
 #define IDR_SinusoidTYPE                130
+#define IDD_DIALOG1                     310
 #define ID_32771                        32771
 #define ID_32772                        32772
 #define ID_coords                       32773
@@ -23,7 +24,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32783
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
